fd5d85d98f0f7990b38fc0ba3c747d51d7a52a5b
Mahaveer <47777013+Mahaveer2@users.noreply.github.com>
2022-07-13 16:25:23 +0500

    Create firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
- 
+ apiKey: "AIzaSyBa5FEp_ODMfg1JE8Fv5eS_XLMzqfjv07o",
+  authDomain: "svelteblog-b6e9d.firebaseapp.com",
+  projectId: "svelteblog-b6e9d",
+  storageBucket: "svelteblog-b6e9d.appspot.com",
+  messagingSenderId: "234249096363",
+  appId: "1:234249096363:web:38fa607733b33555e0bbee",
+  measurementId: "G-NX8RSZB9PC"
 };
 
 const app = initializeApp(firebaseConfig);
